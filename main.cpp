047c766d93e7b6479a350fd316c02b2c0ae9821e
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-07-04 13:21:34 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * MIDIBUS_Tangle.cpp
+ *
+ * Created: 26/06/2021 19:38:07
+ * Author : GuavTek
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
